0f9e4abf62ffc3a0120cf8416ed27f2148601c19
click2cloud-BhushanGupta <bhushan.gupta@click2cloud.net>
2020-06-02 17:17:32 +0530

    Rest api created for fetch single and all user

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+	City      string `json:"city"`
+}
+
+var db *sql.DB
+var err error
+
+func main() {
+	db, err = sql.Open("mysql", "root: tcp(127.0.0.1:3306)/userinfo")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	//init router
+	router := mux.NewRouter()
+
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users/{id}", getUser).Methods("GET")
+	router.HandleFunc("/users", createUser).Methods("POST")
+	router.HandleFunc("/users/{id}", updateUser).Methods("PUT")
+	router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+
+	http.ListenAndServe(":8000", router)
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	result, err := db.Query("SELECT id,firstname,lastname,email,city from users")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	for result.Next() {
+		var user User
+		err := result.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.City)
+		if err != nil {
+			panic(err.Error())
+		}
+		users = append(users, user)
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT * FROM users WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	var user User
+	for result.Next() {
+		err := result.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.City)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("DELETE FROM users WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "User with ID = %s was deleted", params["id"])
+}
+func createUser(w http.ResponseWriter, r *http.Request) {
+	
+	
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	
+}
